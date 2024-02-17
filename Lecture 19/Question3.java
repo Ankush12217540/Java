6a6e4853436a15944236f108edeb718ae5030a0a
Ankush Kumar <115498007+Ankush12217540@users.noreply.github.com>
2024-02-17 23:24:55 +0530

    RiverNameFinder   , FindElement

--- Lecture 19/Question3.java
+++ Lecture 19/Question3.java
@@ -3,7 +3,7 @@ Retype the code below. The class RiverNameFinder has a method findElement(int in
 The findElement method creates an array namesArr containing river names. It accesses the element of namesArr at index and prints the element.
 
 Observe that before accessing the element, the method checks if the index is valid or not by checking if index is greater than zero and less than the size of namesArr. Otherwise, we get an ArrayIndexOutofBoundsException.
-
+ 
 package q10938;
 public class RiverNameFinder {
 	public void findElement(int index) {
