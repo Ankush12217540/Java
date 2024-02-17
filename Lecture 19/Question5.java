c8f96f3cc7a45512dffa2cdb1fd63128dc8f16d1
Ankush Kumar <115498007+Ankush12217540@users.noreply.github.com>
2024-02-17 23:25:41 +0530

    IndexFinder

--- Lecture 19/Question5.java
+++ Lecture 19/Question5.java
@@ -5,7 +5,7 @@ Write a class IndexFinder with public method printIndex that takes two parameter
 Assumptions:
 arr is never null
 Here is an example:
-Cmd Args : 2 2 2 42 2
+Cmd Args : 2 2 2 42 2 
 Indices of the elments matched with the given element 2 is : 
 0
 1
