908277adbb65af781c932623ef7b5c81358fabe8
Ankush Kumar <115498007+Ankush12217540@users.noreply.github.com>
2024-02-17 23:24:17 +0530

    MountainNamePrinter

--- Lecture 19/Question2.java
+++ Lecture 19/Question2.java
@@ -1,6 +1,6 @@
 Create a class MountainNamePrinter with a main method. Create an array with the following names: Nanda Devi, Kamet, K12, Dunagiri.
 
-Iterate over the array and print each name on a separate line.
+Iterate over the array and print each name  on a separate line.
 
 
 package q10937;
