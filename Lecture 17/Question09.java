1b4d3bdffc829b6b7a7030c8ae3183fb87251bd6
Ankush Kumar <115498007+Ankush12217540@users.noreply.github.com>
2024-02-20 21:07:34 +0530

    PrintThreeTimes

--- Lecture 17/Question09.java
+++ Lecture 17/Question09.java
@@ -7,7 +7,7 @@ Note: Please don't change the package name.
 package q10890;
 public class PrintThreeTimes {
 	public static void main(String[] args) {
-		int i = 0;
+		int i = 0; 
 		while (i<3 ) {
 			System.out.println("Thames");
 			i ++ ;
