8c289082acdc611e43f3f4733999b5e336ee3873
Ankush Kumar <115498007+Ankush12217540@users.noreply.github.com>
2024-02-22 23:45:30 +0530

    Identify the error

--- Lecture 24/Question3.java
+++ Lecture 24/Question3.java
@@ -4,7 +4,7 @@ Identify the errors in the given code and correct them.
 package q11159;
 public class Student {
 	private String id;
-	private String name;
+	private String name; 
 	private int age;
 	private char gender;
 }
