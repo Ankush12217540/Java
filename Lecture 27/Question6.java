8f63f668c88ee5fd516bd7522aeeb83812bb04b9
Ankush Kumar <115498007+Ankush12217540@users.noreply.github.com>
2024-02-25 23:50:30 +0530

    escape sequence, employee id

--- Lecture 27/Question6.java
+++ Lecture 27/Question6.java
@@ -9,7 +9,7 @@ public class EscapeSequence {
 	public static void main(String[] args) {
 		String text1 = "insert into Employee(id, name) values(\"10\",\"Meka\");";
 		String text2 = "insert into Employee(id, name) values(\"11\",\"Reka\");";
-		System.out.println(text1);
+		System.out.println(text1); 
 		System.out.println(text2);
 	}
 }
