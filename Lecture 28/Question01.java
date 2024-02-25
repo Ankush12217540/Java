35868e15836600d0d97a1cbceba4fa75a7691329
Ankush Kumar <115498007+Ankush12217540@users.noreply.github.com>
2024-02-25 23:52:18 +0530

    StringMethods

--- Lecture 28/Question01.java
+++ Lecture 28/Question01.java
@@ -4,7 +4,7 @@ For Example:
 "Pentagon".length() returns 8.
 The characters in a string are indexed from 0, just like in an array. The method public char charAt(int index) returns the char value at the specified index. An index ranges from 0 to length() - 1. The first char value of the string is at index 0, the next at index 1, and so on, as for array indexing.
 
-If we want to convert a string to lowercase, we should use public String toLowerCase() method. It returns a new string that has all the characters of the original string in lower case. Similarly, we have a public String toUpperCase() method for converting to upper case.
+If we want to convert a string to lowercase, we should  use public String toLowerCase() method. It returns a new string that has all the characters of the original string in lower case. Similarly, we have a public String toUpperCase() method for converting to upper case.
 
 Click on the  button to see live execution of the above mentioned methods. Make sure to provide your custom text to see the changes.
 
