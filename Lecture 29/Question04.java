726354de2e267204cad1806fd52e326a2164d54b
Ankush Kumar <115498007+Ankush12217540@users.noreply.github.com>
2024-02-26 22:36:22 +0530

    Write a class StringIndexOf with a main method

--- Lecture 29/Question04.java
+++ Lecture 29/Question04.java
@@ -1,7 +1,7 @@
 Write a class StringLastIndexOf with a main method. The method receives one command line argument. Print the index of the last occurrence of the character * in the argument.
 
 For Example:
-Cmd Args : TCA*TX*TX*
+Cmd Args : TCA*TX*TX* 
 9
 
 
