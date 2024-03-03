65b71811ef92a5cd3cdaac04213821f1fcd30f62
Ankush Kumar <115498007+Ankush12217540@users.noreply.github.com>
2024-03-03 23:57:40 +0530

    Create an abstract class Shape, which contains an empty method numberOfSides().

--- Lecture 37/Question3.java
+++ Lecture 37/Question3.java
@@ -1,12 +1,12 @@
 Write a Java program to illustrate the abstract class concept.
 
-Create an abstract class Shape, which contains an empty method numberOfSides().
+
 
 Define three classes named Trapezoid, Triangle and Hexagon extends the class Shape, such that each one of the classes contains only the method numberOfSides(), that contains the number of sides in the given geometrical figure.
 
 Write a class AbstractExample with the main() method, declare an object to the class Shape, create instances of each class and call numberOfSides() methods of each class.
 
-Sample Input and Output:
+Sample Input and Output: 
 Number of sides in a trapezoid are 4
 Number of sides in a triangle are 3
 Number of sides in a hexagon are 6
