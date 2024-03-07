9592e4e3f6e6ce8148f0ec79c0cb91f7f5152f37
Ankush Kumar <115498007+Ankush12217540@users.noreply.github.com>
2024-03-07 23:53:31 +0530

    keyword- static initializer block

--- Lecture 38/Question2.java
+++ Lecture 38/Question2.java
@@ -7,7 +7,7 @@ The code inside the non-static initalizer block is copied inside the constructor
 However, a static initializer block is executed only once when the class is loaded in memory. It has nothing to do with constructor invocation. Even if the constructor is not called to create an object instance, as long as the class is loaded the static initializer blocks get executed.
 
 Since the classes are loaded only once by the system class loader, these static initializer blocks get executed only once.
-
+ 
 See and retype the below code. On execution you will notice that code snippets in the static initializer blocks are executed only once even though we create two instances of class A.
 
 
